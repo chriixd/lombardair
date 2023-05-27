34eda3868098a8d83b90439928e008af87d193a7
Christian <91901457+chriixd@users.noreply.github.com>
2023-05-27 22:15:55 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 var counter = 0;
 var x = 0;
+var num = 1;
 var modalContent = document.getElementById("modal-content");
 var navbarCollapsed = document.getElementById("navbar-collapsed");
 var passengerCount = parseInt(document.getElementById("passenger-count").innerText);
 var isMobileModeOn = window.matchMedia("(max-width: 1450px)");
+var aeroportoPartenza = document.getElementById("provenienza").value;
+var aeroportoArrivo = document.getElementById("destinazione").value;
 
 function modalExpander() {
    if (counter == 0) {
     counter = 1;
     modalContent.classList.remove("hidden");
     modalContent.classList.add("flex");
-    console.log(counter);
    } else {
     counter = 0;
     modalContent.classList.add("hidden");
     modalContent.classList.remove("flex");
-    console.log(counter);
    }
 } 
+
+function modalClose() {
+    counter = 0;
+    modalContent.classList.add("hidden");
+    modalContent.classList.remove("flex");
+}
 
 function addPassenger() {
     if (passengerCount < 5) {
@@ -83,15 +90,24 @@ function validateAr() {
 }
 
 function fillBergamo() {
-    document.getElementById("destinazione").value = "Orio Al Serio (BGY)"
+    document.getElementById("destinazione").value = "Orio Al Serio BGY"
+    setTimeout(() => {
+        $("#provenienza").focus();
+    }, 400);
 }
 
 function fillMalpensa() {
-    document.getElementById("destinazione").value = "Milano Malpensa (MXP)"
+    document.getElementById("destinazione").value = "Milano Malpensa MXP"
+    setTimeout(() => {
+        $("#provenienza").focus();
+    }, 400);
 }
 
 function fillLinate() {
-    document.getElementById("destinazione").value = "Milano Linate (LIN)"
+    document.getElementById("destinazione").value = "Milano Linate LIN"
+    setTimeout(() => {
+        $("#provenienza").focus();
+    }, 400);
 }
 
 $(document).on('click', 'a[href^="#"]', function (event) {
@@ -127,3 +143,87 @@ $( function() {
 		,	duration: "fast"
 	});
 } );
+
+function validateDate() {
+    var dataPartenzaStr = String(document.getElementById("partenza").value);
+    var dataRitornoStr = String(document.getElementById("ritorno").value);
+    var giornoPartenza = parseInt(dataPartenzaStr.slice(0, 2));
+    var mesePartenza = parseInt(dataPartenzaStr.slice(3, 5));
+    var annoPartenza = parseInt(dataPartenzaStr.slice(6, 10));
+    var giornoRitorno = parseInt(dataRitornoStr.slice(0, 2));
+    var meseRitorno = parseInt(dataRitornoStr.slice(3, 5));
+    var annoRitorno = parseInt(dataRitornoStr.slice(6, 10));
+
+    if (annoRitorno < annoPartenza) {
+        document.getElementById("ritorno").value = "";
+        document.getElementById("#ui-datepicker-div").style.display = "block";
+    } else {
+        if (annoRitorno == annoPartenza) {
+            if (meseRitorno < mesePartenza) {
+                document.getElementById("ritorno").value = "";
+                document.getElementById("#ui-datepicker-div").style.display = "block";
+            } else {
+                if (meseRitorno == mesePartenza) {
+                    if (giornoRitorno < giornoPartenza) {
+                        document.getElementById("ritorno").value = "";
+                        document.getElementById("#ui-datepicker-div").style.display = "block";
+                    } else {
+                        console.log("sucess");
+                    }
+                } else {
+                    if (meseRitorno > mesePartenza) {
+                        console.log("success");
+                    }
+                }
+            }
+        } else {
+            if (annoRitorno > annoPartenza) {
+                console.log("success");
+            }
+        }
+    }
+}
+
+function ricercaBiglietti() {
+    var provenienza = String(document.getElementById("provenienza").value).slice(-3);
+    var destinazione = String(document.getElementById("destinazione").value).slice(-3);
+    var partenza = String(document.getElementById("partenza").value);
+    var ritorno = String(document.getElementById("ritorno").value);
+    var viaggiatori = String(document.getElementById("viaggiatori-input").innerText);
+
+    if (document.getElementById("checkbox-ar").checked) {
+        if (provenienza && destinazione && partenza && ritorno != "" && provenienza != destinazione && viaggiatori != "Viaggiatori") {
+            document.getElementById("search-button").href = "#checkbox-ar";
+            document.getElementById("lower-section").style.display = "none";
+            document.getElementById("ticket-oneway-section-wrapper").classList.remove("hidden");
+            for(let num = 1; num < 5; num++) {
+                document.getElementById("depairport" + num).innerText = provenienza;
+            }
+        
+            for(let num = 1; num < 5; num++) {
+                document.getElementById("arrivalairport" + num).innerText = destinazione;
+            }
+            document.getElementById("tickets-title").innerText = "Mostra risultati per " + partenza + " - " + ritorno + ":"; 
+        } else {
+            document.getElementById("search-button").href = "#navbar";
+        }
+    } else {
+        if (provenienza && destinazione && partenza != "" && provenienza != destinazione && viaggiatori != "Viaggiatori") {
+            document.getElementById("search-button").href = "#checkbox-ar";
+            document.getElementById("lower-section").style.display = "none";
+            document.getElementById("ticket-oneway-section-wrapper").classList.remove("hidden");
+            // document.getElementById("tickets-title").classList.remove("hidden");
+            for(let num = 1; num < 5; num++) {
+                document.getElementById("depairport" + num).innerText = provenienza;
+            }
+        
+            for(let num = 1; num < 5; num++) {
+                document.getElementById("arrivalairport" + num).innerText = destinazione;
+            }
+            document.getElementById("tickets-title").innerText = "Mostra risultati per " + partenza+ ":";
+        } else {
+            document.getElementById("search-button").href = "#navbar";
+        }
+    }
+
+}
